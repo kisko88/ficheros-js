e8b12067ed2d0097efad793fcf2cc56af26a641d
kisko88 <kisko_1988@hotmail.com>
2018-08-17 23:28:00 +0200

    Update EjercicioPractico_M1.js

--- EjercicioPractico_M1.js
+++ EjercicioPractico_M1.js
@@ -14,7 +14,7 @@ function aniosBisiestos(){
 	for( i = anio ; i <= fin; i++){
 		if((i % 4 === 0) && (i % 100 != 0)){
 			bisiestos.push(i);
-		}else if ((i % 100 === 0) && (i % 400 === 0)){
+		}elseif ((i % 100 === 0) && (i % 400 === 0)){
 			bisiestos.push(i);
 		}					
 	}				
@@ -27,34 +27,36 @@ B: Cuántas cartas con número par y negras hay en una baraja de poker.
 
 function cartaParNegra(){
 	//palos: Picas, Trebol, Corazones, Diamantes //Values: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
-	var baraja = [ {value:1, palo:"Picas"},{value:2, palo:"Picas"},
-	{value:3, palo:"Picas"},{value:4, palo:"Picas"},
-	{value:5, palo:"Picas"},{value:6, palo:"Picas"},
-	{value:7, palo:"Picas"},{value:8, palo:"Picas"},
-	{value:9, palo:"Picas"},{value:10, palo:"Picas"},
-	{value:11, palo:"Picas"}, {value:12, palo:"Picas"},
-	{value:13, palo:"Picas"},
-	{value:1, palo:"Trebol"},{value:2, palo:"Trebol"},
-	{value:3, palo:"Trebol"},{value:4, palo:"Trebol"},
-	{value:5, palo:"Trebol"},{value:6, palo:"Trebol"},
-	{value:7, palo:"Trebol"},{value:8, palo:"Trebol"},
-	{value:9, palo:"Trebol"},{value:10, palo:"Trebol"},
-	{value:11, palo:"Trebol"}, {value:12, palo:"Trebol"},
-	{value:13, palo:"Trebol"},
-	{value:1, palo:"Corazones"},{value:2, palo:"Corazones"},
-	{value:3, palo:"Corazones"},{value:4, palo:"Corazones"},
-	{value:5, palo:"Corazones"},{value:6, palo:"Corazones"},
-	{value:7, palo:"Corazones"},{value:8, palo:"Corazones"},
-	{value:9, palo:"Corazones"},{value:10, palo:"Corazones"},
-	{value:11, palo:"Corazones"}, {value:12, palo:"Corazones"},
-	{value:13, palo:"Corazones"},
-	{value:1, palo:"Diamantes"},{value:2, palo:"Diamantes"},
-	{value:3, palo:"Diamantes"},{value:4, palo:"Diamantes"},
-	{value:5, palo:"Diamantes"},{value:6, palo:"Diamantes"},
-	{value:7, palo:"Diamantes"},{value:8, palo:"Diamantes"},
-	{value:9, palo:"Diamantes"},{value:10, palo:"Diamantes"},
-	{value:11, palo:"Diamantes"}, {value:12, palo:"Diamantes"},
-	{value:13, palo:"Diamantes"}];
+	var baraja = [ 
+		{value:1, palo:"Picas"},{value:2, palo:"Picas"},
+		{value:3, palo:"Picas"},{value:4, palo:"Picas"},
+		{value:5, palo:"Picas"},{value:6, palo:"Picas"},
+		{value:7, palo:"Picas"},{value:8, palo:"Picas"},
+		{value:9, palo:"Picas"},{value:10, palo:"Picas"},
+		{value:11, palo:"Picas"}, {value:12, palo:"Picas"},
+		{value:13, palo:"Picas"},
+		{value:1, palo:"Trebol"},{value:2, palo:"Trebol"},
+		{value:3, palo:"Trebol"},{value:4, palo:"Trebol"},
+		{value:5, palo:"Trebol"},{value:6, palo:"Trebol"},
+		{value:7, palo:"Trebol"},{value:8, palo:"Trebol"},
+		{value:9, palo:"Trebol"},{value:10, palo:"Trebol"},
+		{value:11, palo:"Trebol"}, {value:12, palo:"Trebol"},
+		{value:13, palo:"Trebol"},
+		{value:1, palo:"Corazones"},{value:2, palo:"Corazones"},
+		{value:3, palo:"Corazones"},{value:4, palo:"Corazones"},
+		{value:5, palo:"Corazones"},{value:6, palo:"Corazones"},
+		{value:7, palo:"Corazones"},{value:8, palo:"Corazones"},
+		{value:9, palo:"Corazones"},{value:10, palo:"Corazones"},
+		{value:11, palo:"Corazones"}, {value:12, palo:"Corazones"},
+		{value:13, palo:"Corazones"},
+		{value:1, palo:"Diamantes"},{value:2, palo:"Diamantes"},
+		{value:3, palo:"Diamantes"},{value:4, palo:"Diamantes"},
+		{value:5, palo:"Diamantes"},{value:6, palo:"Diamantes"},
+		{value:7, palo:"Diamantes"},{value:8, palo:"Diamantes"},
+		{value:9, palo:"Diamantes"},{value:10, palo:"Diamantes"},
+		{value:11, palo:"Diamantes"}, {value:12, palo:"Diamantes"},
+		{value:13, palo:"Diamantes"}
+	];
 
 	var filtro = [];
 
@@ -94,11 +96,8 @@ var palo = carta[1];
 console.log(palo);
 
 /*
-La diferencia es la forma en como lo 
-interpreta JS, y la cantidad de caracteres presentes.
-Para el caso de menor o igual que 9, toma el primer 
-carácter y el otro es el palo.
-Para el caso de mayor o igual a 10, toma el primer 
-carácter como el 1, el segundo como el 2 y el palo 
+La diferencia es la forma en como lo interpreta JS, y el número de caracteres presentes.
+Si es menor o igual que 9, toma como primer carácter número y el otro es el palo.
+En cambio si es mayor o igual a 10, toma el primer carácter como el 1, el segundo como el 0 y el palo 
 lo toma como un tercer carácter.
 */
